e8a50cde9247c1f4222ff481e3e56e722107d271
Dzmitry Ednatsevich <104895057+Dmitriynsv@users.noreply.github.com>
2022-08-03 14:07:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,86 @@
-const todos = [];
-const addTodoButton = document.querySelector("[data-add-todo-button]");
-const template = document.getElementById("template");
+let todos = [];
+const addTodoButton = document.querySelector('[data-add-todo-button]');
+const template = document.querySelector('[data-template]');
 const events = document.getElementById('events');
 const infoTodo = document.querySelector('[data-inter-todo]');
-//1 получить элемент поля(где ввожу текст)  
+const active = document.querySelector('[data-active]');
+const completed = document.querySelector('[data-completed]');
 
 
-addTodoButton.addEventListener("click", () => {
+addTodoButton.addEventListener('click',() =>{
+  const text = infoTodo.value.trim();
 
-  const newTodo = {
-    text: infoTodo.value,
-      };
-    infoTodo.value = '';
-  todos.push(newTodo);
-  render(todos);
+  if (text) {
+    const newTodo = {
+      id: todos.length +1,
+      text,
+      completed: false,
+    }
+    todos.push(newTodo);
+    infoTodo.Value  = '';
+  }
+  infoTodo.focus();
+
+  updateActiveCounter();
+  render ();
 });
 
-function render(todos) {
-  const todoElements = [];
+function createTodo(id,text){
+  const todoItem = document.importNode(template.content, true);
+  const todoText = todoItem.getElementById('title');
+  todoText.textContent = text;
+  infoTodo.value = ''; 
+  const btnRemove = todoItem.querySelector('[data-dtn-remove]');
+  const checkbox = todoItem.querySelector('[data-template-input]');
 
-  todos.forEach((todo) => {
-    todoElements.push(createTodo(todo));
-  });
 
-events.replaceChildren(...todoElements);
+  btnRemove.addEventListener('click', ()=>{
+    todos = todos.filter(todo => todo.id !== id);
+
+   updateActiveCounter(); 
+   updateCompletedCounter();   
+
+    render();
+  })
+  
+  checkbox.addEventListener('click', ()=>{
+    const todo = todos.find((el) => el.id === id);
+    todo.completed = !todo.completed; 
+    updateCompletedCounter();
+    updateActiveCounter();
+  })
+
+  return todoItem;
 }
 
-function createTodo(todo) {
-  const todoElement = document.importNode(template.content, true);
-  const title = todoElement.getElementById("title");
-
-  title.innerText = todo.text;
-
-  return todoElement;
+function clearTodoList(){
+  events.innerHTML = '';  
 }
+
+function appendTodos() {
+  if (todos.length) {
+    todos.forEach(el => {
+      const todo = createTodo(el.id, el.text);
+      events.append (todo);
+    })
+  } else {
+    events.insertAdjacentHTML('beforeend',
+    `<p>No todos...</p>`)
+  }
+}
+
+function render(){
+  clearTodoList();
+  appendTodos();
+}
+
+render();
+
+function updateActiveCounter(){
+  active.innerHTML = todos.filter((el) => el.completed === false).length;
+}
+
+function updateCompletedCounter(){
+  completed.innerHTML = todos.filter((el) => el.completed === true).length;
+}
+
